f272ed0c4f47455fa83a5a5fe74ced9af40cefd3
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-02-18 18:31:57 +0100

    Onto authentication.

--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -1,7 +1,21 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
+import { badRequest } from "~/utils/request.server";
+
+function validateJokeContent(content: string) {
+  if (content.length < 10) {
+    return "That joke is too short";
+  }
+}
+
+function validateJokeName(name: string) {
+  if (name.length < 3) {
+    return "That joke's name is too short";
+  }
+}
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const form = await request.formData();
@@ -10,10 +24,28 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   // we do this type check to be extra sure and to make TypeScript happy
   // we'll explore validation next!
   if (typeof content !== "string" || typeof name !== "string") {
-    throw new Error("Form not submitted correctly.");
+    // throw new Error("Form not submitted correctly.");
+    return badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: "Form not submitted correctly.",
+    });
   }
 
+  const fieldErrors = {
+    content: validateJokeContent(content),
+    name: validateJokeName(name),
+  };
+
   const fields = { content, name };
+  // not sure I understand this first line
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+      formError: null,
+    });
+  }
 
   const joke = await db.joke.create({
     // data: { name, content },
@@ -23,21 +55,59 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 };
 
 export default function NewJokeRoute() {
+  const actionData = useActionData<typeof action>();
+
   return (
     <div>
       <p>Add your own hilarious joke</p>
       <form method="post">
         <div>
           <label>
-            Name: <input type="text" name="name" />
+            Name:{" "}
+            <input
+              defaultValue={actionData?.fields?.name}
+              type="text"
+              name="name"
+              aria-invalid={Boolean(actionData?.fieldErrors?.name)}
+              aria-errormessage={
+                actionData?.fieldErrors?.name ? "name-error" : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.name ? (
+            <p className="form-validation-error" id="name-error" role="alert">
+              {actionData.fieldErrors.name}
+            </p>
+          ) : null}
         </div>
         <div>
           <label>
-            Content: <textarea name="content" />
+            Content:{" "}
+            <textarea
+              defaultValue={actionData?.fields?.content}
+              name="content"
+              aria-invalid={Boolean(actionData?.fieldErrors?.content)}
+              aria-errormessage={
+                actionData?.fieldErrors?.content ? "content-error" : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.content ? (
+            <p
+              className="form-validation-error"
+              id="content-error"
+              role="alert"
+            >
+              {actionData.fieldErrors.content}
+            </p>
+          ) : null}
         </div>
         <div>
+          {actionData?.formError ? (
+            <p className="form-validation-error" role="alert">
+              {actionData.formError}
+            </p>
+          ) : null}
           <button type="submit" className="button">
             Add
           </button>
@@ -46,3 +116,9 @@ export default function NewJokeRoute() {
     </div>
   );
 }
+
+/* Personal notes
+I'm honestly thinking I should use SQLite for my project. Perhaps even redo it in Remix to that end. Trying to make it fit with Prisma too. Having the database inside my project at that little scope is definitely going to make it a whole lot faster. Then I can go back to Next.js once I need the project to reach real production grade.
+...
+I mean if you make a little website do you even need SQL if users won't be able to do some CRUD on it? Do you even need an actual database? Can't you have you own files ready up in there just like documentations do? It's all a matter of scale, and I ought to be able to work at any scale.
+*/
