c85e83ce45a4123e252345145c564ee36fcaea2b
Luther Tchofo Safo <nintendobserver@gmail.com>
2024-03-01 15:48:08 +0100

    Imported and implemented remember.

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -4,3 +4,9 @@ import { singleton } from "./singleton.server";
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
 export const db = singleton("prisma", () => new PrismaClient());
+
+//
+
+import { remember } from "@epic-web/remember";
+
+export const prisma = remember("prisma", () => new PrismaClient());
